d562e6e983e107aa9ec615525189f79a9c31093b
Nicola Macoir <nicola.macoir@gmail.com>
2020-12-20 14:38:04 +0100

    fixed cors issue

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var cors = require('cors')
 
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(cors())
 
 var routes = require('./api/routes/beluxRoutes'); //importing route
 routes(app); //register the route
