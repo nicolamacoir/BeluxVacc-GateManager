758ef96eab61f3d87a6eb078da2aa73d0330be8b
Nicola Macoir <nicola.macoir@gmail.com>
2021-01-11 22:11:22 +0100

    major update: general refactoring + ELLX

--- api/controllers/beluxController.js
+++ api/controllers/beluxController.js
@@ -1,13 +1,14 @@
 const fetch = require("node-fetch");
 var _ = require('underscore');
 var Datastore = require('nedb')
-var db = new Datastore();
 var f = require('./helpFunctions.js')
-var json = require('../data/gates.json');
+
 
 var DEBUG = false;
 
 // INJECT DATA IN DATABASE
+var db = new Datastore();
+var json = require('../data/gates.json');
 db.insert(json, function(err, result){
      if(!err){
         if (DEBUG) console.log("succesfully imported!")
@@ -19,8 +20,12 @@ let active_clients = null;
 let monitored_clients = {}
 let last_updated = Date.now()
 
-const location_brussels = {"latitude": 50.902, "longitude": 4.485}
-const location_charleroi = {"latitude": 50.4647, "longitude": 4.4611}
+const airports_of_interest = ["EBBR", "ELLX"]
+const location_coordinates = {
+    "EBBR" : {"latitude": 50.902, "longitude": 4.485},
+    "EBCI" : {"latitude": 50.4647, "longitude": 4.4611},
+    "ELLX" : {"latitude": 49.6313, "longitude": 6.2157}
+}
 
 async function get_all_gates(){
     var gates = await new Promise((resolve, reject) => {
@@ -32,11 +37,23 @@ async function get_all_gates(){
     return gates
 }
 
-async function get_all_possible_gates_for(ac, apron){
-    if(ac == "A388"){
+async function get_all_gates_for_airport(airport){
+    var gates = await new Promise((resolve, reject) => {
+        db.find({"airport":airport}, {"_id": 0, "__v":0}).sort({occupied : -1, apron:1 }).exec((err, count) => {
+            if (err) reject(err);
+            resolve(count);
+        });
+    });
+    return gates
+}
+
+async function get_all_possible_gates_for(airport, ac, apron){
+    var aprons = apron[0], backup_aprons = apron[1]
+
+    if(airport == "EBBR" && ac == "A388"){
         var super_gates = ["233R", "322", "328"]
         var gates = await new Promise((resolve, reject) => {
-            db.find({"gate":{ $in: super_gates}, "occupied":false}, {"_id": 0, "__v":0}).sort({apron: 1}).exec((err, result) => {
+            db.find({"airport":airport, "gate":{ $in: super_gates}, "occupied":false}, {"_id": 0, "__v":0}).sort({apron: 1}).exec((err, result) => {
                 if (err) reject(err);
                 resolve(result);
             });
@@ -45,17 +62,26 @@ async function get_all_possible_gates_for(ac, apron){
     }
     /* Find all gates on specific apron */
     var gates = await new Promise((resolve, reject) => {
-        db.find({"apron": { $in: apron}, "occupied":false}, {"_id": 0, "__v":0}).sort({apron: 1}).exec((err, result) => {
+        db.find({"airport":airport, "apron": { $in: aprons}, "occupied":false}, {"_id": 0, "__v":0}).sort({apron: 1}).exec((err, result) => {
             if (err) reject(err);
             resolve(result);
         });
     });
+    /* When all gates are occupied, use backup apron*/
+    if(gates.length == 0){
+        gates = await new Promise((resolve, reject) => {
+            db.find({"airport":airport, "apron": { $in: backup_aprons}, "occupied":false}, {"_id": 0, "__v":0}).sort({apron: 1}).exec((err, result) => {
+                if (err) reject(err);
+                resolve(result);
+            });
+        });
+    }
     return gates
 }
 
-async function get_gate_for_gateid(gate_id){
+async function get_gate_for_gateid(airport, gate_id){
     var gate = await new Promise((resolve, reject) => {
-        db.findOne({"gate": gate_id}, {"_id": 0, "__v":0}, (err, result) => {
+        db.findOne({"airport":airport, "gate": gate_id}, {"_id": 0, "__v":0}, (err, result) => {
             if (err) reject(err);
             resolve(result);
         });
@@ -73,10 +99,10 @@ async function get_gate_for_callsign(callsign,ac_type=null){
     return gate
 }
 
-async function set_gate_to_callsign(gate_id, callsign){
+async function set_gate_to_callsign(airport, gate_id, callsign){
 
     /* Check if gate is not occupied */
-    var cur_gate = await get_gate_for_gateid(gate_id);
+    var cur_gate = await get_gate_for_gateid(airport, gate_id);
     if (cur_gate.assigned_to != "none"){ 
         if (DEBUG) console.log("ERR: gate " + gate_id + " already assigned to " + cur_gate.assigned_to);
         return "ERR: gate already occupied"
@@ -91,7 +117,7 @@ async function set_gate_to_callsign(gate_id, callsign){
     
     /* Get gate */
     var gate = await new Promise((resolve, reject) => {
-        db.findOne({"gate": gate_id}, (err, result) => {
+        db.findOne({"airport":airport, "gate": gate_id}, (err, result) => {
             if (err) reject(err);
             resolve(result);
         });
@@ -103,7 +129,7 @@ async function set_gate_to_callsign(gate_id, callsign){
         gate.occupied = true;
         gate.assigned_to = callsign;
         var result = await new Promise((resolve, reject) => {
-            db.update({"gate": gate_id}, gate, function(err, result){
+            db.update({"airport":airport, "gate": gate_id}, gate, function(err, result){
                 if (err) reject(err);
                 resolve(gate);
             });
@@ -115,9 +141,9 @@ async function set_gate_to_callsign(gate_id, callsign){
     }
 }
 
-async function clear_gate(gate_id){
+async function clear_gate(airport, gate_id){
     var gate = await new Promise((resolve, reject) => {
-        db.findOne({"gate": gate_id}, (err, result) => {
+        db.findOne({"airport":airport, "gate": gate_id}, (err, result) => {
             if (err) reject(err);
             resolve(result);
         });
@@ -125,7 +151,7 @@ async function clear_gate(gate_id){
     gate.occupied = false;
     gate.assigned_to = "none";
     var result = await new Promise((resolve, reject) => {
-        db.update({"gate": gate_id}, gate, function(err, result){
+        db.update({"airport":airport, "gate": gate_id}, gate, function(err, result){
             if (err) reject(err);
             resolve("OK");
         });
@@ -134,19 +160,19 @@ async function clear_gate(gate_id){
 }
 
 
-async function request_gate_for(callsign, origin, ac){
-    var aprons = f.get_valid_aprons(callsign,origin, ac);
-    return await request_gate_on_apron(callsign, ac, aprons);
+async function request_gate_for(airport, callsign, origin, ac){
+    var aprons = f.get_valid_aprons(airport, callsign,origin, ac);
+    return await request_gate_on_apron(airport, callsign, ac, aprons);
 }
 
-async function request_gate_on_apron(callsign, ac, apron){
-    var gates = await get_all_possible_gates_for(ac, apron);
+async function request_gate_on_apron(airport, callsign, ac, apron){
+    var gates = await get_all_possible_gates_for(airport, ac, apron);
     var temp_gate = gates[Math.floor(Math.random() * gates.length)];
 
-    var result = await set_gate_to_callsign(temp_gate["gate"], callsign)
+    var result = await set_gate_to_callsign(airport, temp_gate["gate"], callsign)
     while(typeof result === 'string' && result.startsWith("ERR")){
         var temp_gate = gates[Math.floor(Math.random() * gates.length)];
-        var result = await set_gate_to_callsign(temp_gate["gate"], callsign)
+        var result = await set_gate_to_callsign(airport, temp_gate["gate"], callsign)
     }
     return result
 }
@@ -163,7 +189,10 @@ async function load_active_clients(){
         for(i=0; i< out["clients"].length;i++){
             var client =  out["clients"][i]
             var location_client = {"latitude": client["latitude"], "longitude": client["longitude"]} 
-            if ((client["planned_depairport"] == "EBBR" || client["planned_destairport"] == "EBBR") && f.worldDistance(location_client, location_brussels) < 300){
+            if (airports_of_interest.includes(client["planned_depairport"]) && f.worldDistance(location_client, location_coordinates[client["planned_depairport"]]) < 300){
+                clients.push(client)
+            }
+            else if (airports_of_interest.includes(client["planned_destairport"])  && f.worldDistance(location_client, location_coordinates[client["planned_destairport"]]) < 300){
                 clients.push(client)
             }
         }
@@ -196,10 +225,10 @@ async function process_clients(clients){
             AC_code = client["planned_aircraft"].split("/")[1];
         }
 
+        var on_dep_ground = (client["planned_depairport"] in f.airport_zones ?  f.is_on_zone(f.airport_zones[client["planned_depairport"]], lat, long, altitude) : false);
+        var on_arr_ground = (client["planned_destairport"] in f.airport_zones ?  f.is_on_zone(f.airport_zones[client["planned_destairport"]], lat, long, altitude) : false);
 
-        var on_ground = f.is_on_brussels_ground(lat, long, altitude);
-
-        if (on_ground){
+        if (on_dep_ground || on_arr_ground){
             var closestGate = f.get_gate_for_position(lat, long);
             // CHECK gate reservation OK
             if (closestGate == null){
@@ -207,51 +236,52 @@ async function process_clients(clients){
                 if ( callsign in monitored_clients && monitored_clients[callsign] == "AUTO-DEP"){
                     var gate = await get_gate_for_callsign(callsign);
                     if(gate!=null){
-                        var result = await clear_gate(gate["gate"]); 
+                        var result = await clear_gate(gate["airport"], gate["gate"]); 
                         delete monitored_clients[callsign]
                         if (DEBUG) console.log("deleted " + callsign)
                     }
                 }
             }else{
                 // AC is at gate
-                var cur_gate = await get_gate_for_callsign(callsign, AC_code);
+                var cur_gate = await get_gate_for_callsign(callsign);
                 if (cur_gate == null){
-                    let gate = await get_gate_for_gateid(closestGate["gate"]);
+                    let gate = await get_gate_for_gateid(closestGate["airport"], closestGate["gate"]);
                     if(gate.occupied == true){
                         /* Gate was already assigned => double booking
                            Make new reservation for that client */
                         var other_callsign = gate.assigned_to;
                         var other_apron = gate.apron;
+                        var other_airport = gate.airport
                         clear_gate(gate["gate"]);
-                        request_gate_on_apron(other_callsign, AC_code, [other_apron]);
+                        request_gate_on_apron(other_airport, other_callsign, AC_code, [other_apron]);
                     }
-                    var result = set_gate_to_callsign(gate["gate"], callsign); 
+                    var result = set_gate_to_callsign(gate["airport"],gate["gate"], callsign); 
                     monitored_clients[callsign] = "AUTO-DEP"
                     load_active_clients();
                 }
                 status = "at_gate"
             }
         }else{
-            if(client["planned_depairport"] == "EBBR"){
+            if(airports_of_interest.includes(client["planned_depairport"])){
                 status = "departed"
                 if ( callsign in monitored_clients && monitored_clients[callsign] == "AUTO-DEP"){
                     var gate = await get_gate_for_callsign(callsign)
                     if(gate!=null){
-                        var result = await clear_gate(gate["gate"]); 
+                        var result = await clear_gate(gate["airport"], gate["gate"]); 
                         delete monitored_clients[callsign]
                         if (DEBUG) console.log("deleted " + callsign)
                     }
                 }
             }else{
                 var location_client = {"latitude": lat, "longitude": long} 
-                arr_distance = parseInt(f.worldDistance(location_client, location_charleroi))
+                arr_distance = parseInt(f.worldDistance(location_client, location_coordinates[client["planned_destairport"]]))
                 if(parseInt(ground_speed) < 50)
                     /* not yet departed from origin*/
                     continue
                 if (arr_distance < 150){
-                    var cur_gate = await get_gate_for_callsign(callsign, AC_code);
+                    var cur_gate = await get_gate_for_callsign(callsign);
                     if (cur_gate == null){
-                        var result = await request_gate_for(callsign, client["planned_depairport"], AC_code)
+                        var result = await request_gate_for(client["planned_destairport"], callsign, client["planned_depairport"], AC_code)
                         monitored_clients[callsign] = "AUTO_ARR"
                     }
                 }
@@ -260,12 +290,13 @@ async function process_clients(clients){
                 status = "arriving"
             }
         }
-        var result = await get_gate_for_callsign(callsign, AC_code);
+        var result = await get_gate_for_callsign(callsign);
         var gate = (result== null ? "": (result["gate"])) 
         output_clients.push(
-            {"type"     : (client["planned_depairport"] == "EBBR" ? "D":"A"),
+            {"type"     : (airports_of_interest.includes(client["planned_depairport"]) ? "D":"A"),
              "callsign" : callsign, 
-             "airport"  : (client["planned_depairport"] == "EBBR"?client["planned_destairport"]:client["planned_depairport"]),
+             "dest_airport"  : client["planned_destairport"],
+             "dep_airport"   : client["planned_depairport"],
              "ac"       : AC_code,
              "status"   : status,
              "distance" : arr_distance,
@@ -290,7 +321,7 @@ function bookkeep_clients(){
         if (!found){
             if (DEBUG) console.log("cleaning up " + key)
             var gate = await get_gate_for_callsign(key);
-            var result = await clear_gate(gate["gate"]);
+            var result = await clear_gate(gate["airport"], gate["gate"]);
             delete monitored_clients[key]
         }
     });
@@ -308,22 +339,31 @@ exports.list_all_gates = async function(req, res) {
     res.json(gates);
 };
 
-/* /POST/all_gates */
+/* /GET/all_gates/:airport/ */
+exports.list_all_gates_for_airport = async function(req, res) {
+    airport = req.params["airport"].toUpperCase();
+    var gates = await get_all_gates_for_airport(airport)
+    res.json(gates);
+};
+
+/* /POST/all_gates/:airport/ */
 exports.list_all_valid_gates = async function(req, res) {
+    airport = req.params["airport"].toUpperCase();;
     callsign = req.body.callsign;
     origin = req.body.origin;
     ac = req.body.aircraft;
 
-    var aprons = f.get_valid_aprons(callsign, origin, ac)
-    var gates =  await get_all_possible_gates_for(ac, aprons)
+    var aprons = f.get_valid_aprons(airport, callsign, origin, ac)
+    var gates =  await get_all_possible_gates_for(airport, ac, aprons)
     res.json(gates)
 };
 
 /* /GET/get_gate/:gateid*/
 exports.get_gate_for_id = async function(req, res){
+    airport = req.params["airport"].toUpperCase();;
     gate_id = req.params["gateid"];
 
-    var gate = await get_gate_for_gateid(gate_id)
+    var gate = await get_gate_for_gateid(airport, gate_id)
     res.json(gate);
 };
 
@@ -338,10 +378,11 @@ exports.get_gate_for_callsign = async function(req, res){
 /* /POST/request_gate */
 exports.request_gate = async function(req, res){
     callsign = req.body.callsign;
+    airport = req.body.airport.toUpperCase();;
     origin = req.body.origin;
     ac = req.body.aircraft;
 
-    var result = await request_gate_for(callsign, origin, ac);
+    var result = await request_gate_for(airport,callsign, origin, ac);
     res.json(result);
     monitored_clients[callsign] = "MANUAL"
 };
@@ -352,8 +393,8 @@ exports.change_gate = async function(req, res){
     requested_gateid = req.body.gate_id;
 
     var old_gate = await get_gate_for_callsign(callsign);
-    var result = await clear_gate(old_gate["gate"]);
-    var new_gate = await set_gate_to_callsign(requested_gateid, callsign);
+    var result = await clear_gate(old_gate["airport"], old_gate["gate"]);
+    var new_gate = await set_gate_to_callsign(old_gate["airport"],requested_gateid, callsign);
     if(typeof new_gate === 'string' && new_gate.startsWith("ERR")){
         res.status(500).send(
         {
@@ -373,12 +414,13 @@ exports.change_gate = async function(req, res){
 exports.toggle_reservation = async function(req, res){
     callsign = req.body.callsign;
     var requested_gateid = req.params["gateid"];
+    var requested_airport = req.params["airport"].toUpperCase();;
 
-    var gate = await get_gate_for_gateid(requested_gateid);
+    var gate = await get_gate_for_gateid(requested_airport,requested_gateid);
     if (gate.occupied == true){
-        var result = await clear_gate(requested_gateid);
+        var result = await clear_gate(requested_airport,requested_gateid);
     }else{
-        var result = await set_gate_to_callsign(requested_gateid, callsign);
+        var result = await set_gate_to_callsign(requested_airport, requested_gateid, callsign);
     }
     monitored_clients[callsign] = "MANUAL";
     res.json(result);
@@ -387,7 +429,11 @@ exports.toggle_reservation = async function(req, res){
 
 /* /GET/get_clients */
 exports.get_active_clients = function(req, res){
-    res.json({"updated": last_updated, "clients": active_clients});
+    airport = req.params["airport"].toUpperCase();
+    filtered_clients = active_clients.filter(function(el){
+        return el.dest_airport == airport || el.dep_airport == airport
+    });
+    res.json({"updated": last_updated, "clients": filtered_clients});
 }
 
 /* /GET/force_get_clients */
--- api/routes/beluxRoutes.js
+++ api/routes/beluxRoutes.js
@@ -5,12 +5,15 @@ module.exports = function(app) {
 
     app.route('/gates')
     .get(belux.list_all_gates)
+
+    app.route('/gates/:airport')
+    .get(belux.list_all_gates_for_airport)
     .post(belux.list_all_valid_gates)
 
     app.route('/get_gate/')
     .post(belux.get_gate_for_callsign)
 
-    app.route('/get_gate/:gateid')
+    app.route('/get_gate/:airport/:gateid')
     .get(belux.get_gate_for_id)
 
     app.route('/request_gate')
@@ -19,10 +22,10 @@ module.exports = function(app) {
     app.route('/change_gate')
     .post(belux.change_gate)
 
-    app.route('/toggle_reservation/:gateid')
+    app.route('/toggle_reservation/:airport/:gateid')
     .post(belux.toggle_reservation)
 
-    app.route('/get_clients')
+    app.route('/get_clients/:airport')
     .get(belux.get_active_clients)
 
     app.route('/force_get_clients')
